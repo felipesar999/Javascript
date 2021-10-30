0c275b3fb191791029aff20a503eef240cad7fdc
Felipe <felipesar999@gmail.com>
2021-10-30 12:43:03 -0500

    inicios de javascript

---
+++ js/apps.js
@@ -0,0 +1,178 @@
+const data = 
+{​​​​​​  "estudiantes": [
+  {​​​​​​
+    nombre: "Fulanito de tal",
+    github: "https://github.com/mirepositorio",
+    neocites: "miportafolio.neocities.org",
+    email: "estudiante@dominio.com",
+    telefono: "3125678900",
+    foto: "https://blush.design/api/download?shareUri=cK1sApjjMV6Db33n&c=Bottom_0%7E393f82-0.1%7E393f82-0.2%7E393f82_Hair_0%7Ee8e1e1-0.1%7Ee8e1e1-0.2%7E4a312c_Skin_0%7Eb28b67-0.1%7Eb28b67-0.2%7E57331f_Top_0%7Effa434-0.1%7E43d26c-0.2%7E43d26c&w=800&h=800&fm=png",
+    actividades: [
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​,
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​,
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​
+    ],
+    valoraciones: {​​​​​​
+      valhtml: "3%",
+      valcss: "4%",
+      valjs: "6%",
+      valui: "4%",
+      valux: "9%"
+    }​​​​​​,
+    calificacion: "54%"
+  }​​​​​​,
+   {​​​​​​
+    nombre: "Fulanito de tal",
+    github: "https://github.com/mirepositorio",
+    neocites: "miportafolio.neocities.org",
+    email: "estudiante@dominio.com",
+    telefono: "3125678900",
+    foto: "https://blush.design/api/download?shareUri=Uz2obBVypgQgWsyE&c=Bottom_0%7E89c5cc_Hair_0%7E2c1b18_Skin_0%7E57331f_Top_0%7Eff4133&w=800&h=800&fm=png",
+    actividades: [
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​,
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​,
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​
+    ],
+    valoraciones: {​​​​​​
+      valhtml: "3%",
+      valcss: "4%",
+      valjs: "6%",
+      valui: "4%",
+      valux: "9%"
+    }​​​​​​,
+    calificacion: "54%"
+  }​​​​​​, {​​​​​​
+    nombre: "Fulanito de tal",
+    github: "https://github.com/mirepositorio",
+    neocites: "miportafolio.neocities.org",
+    email: "estudiante@dominio.com",
+    telefono: "3125678900",
+    foto: "https://blush.design/api/download?shareUri=W2IJE_4jDrb2MlQa&c=Bottom_0%7E2b44ff_Hair_0%7Ee8e1e1_Skin_0%7E915b3c_Top_0%7Ef2f2f2&w=800&h=800&fm=png",
+    actividades: [
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​,
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​,
+      {​​​​​​
+        actividad: "actividad 1",
+        estadouiux: "5%",
+        estadotecnico: "2%"
+      }​​​​​​
+    ],
+    valoraciones: {​​​​​​
+      valhtml: "3%",
+      valcss: "4%",
+      valjs: "6%",
+      valui: "4%",
+      valux: "9%"
+    }​​​​​​,
+    calificacion: "54%"
+  }​​​​​​
+]
+}​​​​​​
+
+
+
+const Datos = `<div class="main">
+<div class="Names">
+  <div class="Names_name_order">
+    <div class="Names_name">
+      <h2>nombre</h2>
+    </div>
+
+    <div class="Names_Enlaces">
+      <a class="Names_Enlaces_data" href="#"><i class="fab fa-github"></i> GitHub</a>
+      <a class="Names_Enlaces_data" href="#"><i class="fas fa-network-wired"></i> Neocities</a>
+      <a class="Names_Enlaces_data" href="#"><i class="fas fa-envelope-open-text"></i> Email</a>
+      <a class="Names_Enlaces_data" href="#"><i class="fab fa-whatsapp"></i> Whatsapp</a>
+    </div>
+  </div>
+  <img class="Names_Imagen_Img" src="../img/FitzInthePark.png" alt="">`
+
+  const Actividades =
+`</div>
+<div class="Activity">
+  <table class="tabla">
+    <tr>
+      <th>Actividades</th>
+      <th>% UX/UI</th>
+      <th>%Dev</th>
+    </tr>
+    <tr>
+      <td>Actividad 1</td>
+      <td>10%</td>
+      <td>5%</td>
+    </tr>
+    <tr>
+      <td>Actividad 2</td>
+      <td>10%</td>
+      <td>5%</td>
+    </tr>
+    <tr>
+      <td>Actividad 3</td>
+      <td>10%</td>
+      <td>5%</td>
+    </tr>
+  </table>
+</div>`
+
+const Totales =
+`
+<div class="Totals">
+  <div class="button">
+    <span>100%</span>
+    <a href="#">Enlace</a>
+  </div>
+  <div class="button">
+    <span>100%</span>
+    <a href="#">Enlace</a>
+  </div>
+  <div class="button">
+    <span>100%</span>
+    <a href="#">Enlace</a>
+  </div>
+  <div class="button">
+    <span>100%</span>
+    <a href="#">Enlace</a>
+  </div>
+  <div class="button">
+    <span>100%</span>
+    <a href="#">Enlace</a>
+  </div>
+  <div class="button">
+    <span>100%</span>
+    <a href="#">Enlace</a>
+  </div>
+</div>
+</div>`;
+const p = document.getElementById("apps").innerHTML = Datos + Actividades + Totales;
