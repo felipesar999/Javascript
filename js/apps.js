f7d53f48bdd6f8e7b2a55b488132300dbd1160fc
Felipe <felipesar999@gmail.com>
2021-11-06 11:30:04 -0500

    union js con html

--- js/apps.js
+++ js/apps.js
@@ -1,71 +1,71 @@
-const data = 
-{​​​​​​  "estudiantes": [
-  {​​​​​​
-    nombre: "Fulanito de tal",
+const test = (document.querySelector("body").onload = function() { const pmdd = mostrarficha()});
+const estudiantes  = [ {
+  nombre: "Fulanito de tal",
     github: "https://github.com/mirepositorio",
     neocites: "miportafolio.neocities.org",
     email: "estudiante@dominio.com",
     telefono: "3125678900",
     foto: "https://blush.design/api/download?shareUri=cK1sApjjMV6Db33n&c=Bottom_0%7E393f82-0.1%7E393f82-0.2%7E393f82_Hair_0%7Ee8e1e1-0.1%7Ee8e1e1-0.2%7E4a312c_Skin_0%7Eb28b67-0.1%7Eb28b67-0.2%7E57331f_Top_0%7Effa434-0.1%7E43d26c-0.2%7E43d26c&w=800&h=800&fm=png",
     actividades: [
-      {​​​​​​
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​,
-      {​​​​​​
+      },
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​,
-      {​​​​​​
+      },
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​
+      }
     ],
-    valoraciones: {​​​​​​
+    valoraciones: {
       valhtml: "3%",
       valcss: "4%",
       valjs: "6%",
       valui: "4%",
       valux: "9%"
-    }​​​​​​,
+    },
     calificacion: "54%"
-  }​​​​​​,
-   {​​​​​​
-    nombre: "Fulanito de tal",
+  },
+   {
+     nombre: "Fulanito de tal",
     github: "https://github.com/mirepositorio",
     neocites: "miportafolio.neocities.org",
     email: "estudiante@dominio.com",
     telefono: "3125678900",
     foto: "https://blush.design/api/download?shareUri=Uz2obBVypgQgWsyE&c=Bottom_0%7E89c5cc_Hair_0%7E2c1b18_Skin_0%7E57331f_Top_0%7Eff4133&w=800&h=800&fm=png",
     actividades: [
-      {​​​​​​
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​,
-      {​​​​​​
+      },
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​,
-      {​​​​​​
+      },
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​
+      }
     ],
-    valoraciones: {​​​​​​
+    valoraciones: {
       valhtml: "3%",
       valcss: "4%",
       valjs: "6%",
       valui: "4%",
       valux: "9%"
-    }​​​​​​,
+    },
     calificacion: "54%"
-  }​​​​​​, {​​​​​​
+  },
+  {
     nombre: "Fulanito de tal",
     github: "https://github.com/mirepositorio",
     neocites: "miportafolio.neocities.org",
@@ -73,54 +73,56 @@ const data =
     telefono: "3125678900",
     foto: "https://blush.design/api/download?shareUri=W2IJE_4jDrb2MlQa&c=Bottom_0%7E2b44ff_Hair_0%7Ee8e1e1_Skin_0%7E915b3c_Top_0%7Ef2f2f2&w=800&h=800&fm=png",
     actividades: [
-      {​​​​​​
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​,
-      {​​​​​​
+      },
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​,
-      {​​​​​​
+      },
+      {
         actividad: "actividad 1",
         estadouiux: "5%",
         estadotecnico: "2%"
-      }​​​​​​
+      }
     ],
-    valoraciones: {​​​​​​
+    valoraciones: {
       valhtml: "3%",
       valcss: "4%",
       valjs: "6%",
       valui: "4%",
       valux: "9%"
-    }​​​​​​,
+    },
     calificacion: "54%"
-  }​​​​​​
-]
-}​​​​​​
+  }
+]; 
+ 
 
 
 
-const Datos = `<div class="main">
+
+function Datos(estudiantes) {
+  return `<div class="main">
 <div class="Names">
   <div class="Names_name_order">
     <div class="Names_name">
-      <h2>nombre</h2>
+      <h2>${estudiantes.nombre}</h2>
     </div>
 
     <div class="Names_Enlaces">
-      <a class="Names_Enlaces_data" href="#"><i class="fab fa-github"></i> GitHub</a>
-      <a class="Names_Enlaces_data" href="#"><i class="fas fa-network-wired"></i> Neocities</a>
-      <a class="Names_Enlaces_data" href="#"><i class="fas fa-envelope-open-text"></i> Email</a>
-      <a class="Names_Enlaces_data" href="#"><i class="fab fa-whatsapp"></i> Whatsapp</a>
+      <a class="Names_Enlaces_data" href="#"><i class="fab fa-github"></i> ${estudiantes.github}</a>
+      <a class="Names_Enlaces_data" href="#"><i class="fas fa-network-wired"></i> ${estudiantes.neocites}</a>
+      <a class="Names_Enlaces_data" href="#"><i class="fas fa-envelope-open-text"></i> ${estudiantes.email}</a>
+      <a class="Names_Enlaces_data" href="#"><i class="fab fa-whatsapp"></i> ${estudiantes.telefono }</a>
     </div>
   </div>
-  <img class="Names_Imagen_Img" src="../img/FitzInthePark.png" alt="">`
-
-  const Actividades =
-`</div>
+  <img class="Names_Imagen_Img" src="${estudiantes.foto}" alt="">
+  
+  
+  </div>
 <div class="Activity">
   <table class="tabla">
     <tr>
@@ -144,35 +146,42 @@ const Datos = `<div class="main">
       <td>5%</td>
     </tr>
   </table>
-</div>`
+</div>
 
-const Totales =
-`
+
 <div class="Totals">
   <div class="button">
-    <span>100%</span>
-    <a href="#">Enlace</a>
+    <span>${estudiantes.valoraciones.valhtml}</span>
+    <a href="#">HTML</a>
   </div>
   <div class="button">
-    <span>100%</span>
-    <a href="#">Enlace</a>
+    <span>${estudiantes.valoraciones.valcss}</span>
+    <a href="#">CSS</a>
   </div>
   <div class="button">
-    <span>100%</span>
-    <a href="#">Enlace</a>
+    <span>${estudiantes.valoraciones.valjs}</span>
+    <a href="#">JS</a>
   </div>
   <div class="button">
-    <span>100%</span>
-    <a href="#">Enlace</a>
+    <span>${estudiantes.valoraciones.valui}</span>
+    <a href="#">UI</a>
   </div>
   <div class="button">
-    <span>100%</span>
-    <a href="#">Enlace</a>
+    <span>${estudiantes.valoraciones.valux}</span>
+    <a href="#">UX</a>
   </div>
   <div class="button">
-    <span>100%</span>
-    <a href="#">Enlace</a>
+    <span>${estudiantes.calificacion}</span>
+    <a href="#">Total</a>
   </div>
 </div>
-</div>`;
-const p = document.getElementById("apps").innerHTML = Datos + Actividades + Totales;
+</div>
+<br>`;
+}
+
+function mostrarficha(){
+  console.log(Datos)
+const p2 = document.getElementById("apps").innerHTML = `${estudiantes.map(Datos).join("")}`;
+return p2
+} 
+const p8 = document.getElementById("apps").innerHTML = `${estudiantes.map(Datos).join("")}`;
