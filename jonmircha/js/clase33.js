eba470f976864d4e5db507969ef0c7bdbd2c2863
Felipe Zapata <soporte@feriadelbrasier.com.co>
2022-04-27 17:24:19 -0500

    clases hasta la 31

---
+++ jonmircha/js/clase33.js
@@ -0,0 +1,10 @@
+import {PI,usuario} from "./clase33constantes.js"
+import {aritmetica} from "./clase33aritmetica.js"
+
+console.log("Modulos en clase 33")
+
+console.log(PI, usuario)
+
+console.log(aritmetica.sumar(1,3))
+
+//saludar()
---
+++ jonmircha/js/clase33constantes.js
@@ -0,0 +1,8 @@
+export const PI = Math.PI
+
+export let usuario = "jon";
+const password = "qwerty";
+
+export default function saludar(){
+    console.log("Hola, un saludo de los Modulos +ES6")
+}
