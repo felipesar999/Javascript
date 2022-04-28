44884e446ad4f999220253c86994a2757caaa434
felipesar999 <felipesar999@gmail.com>
2022-04-28 07:31:04 -0500

    Clase 34 Ej2

--- jonmircha/js/clase34.js
+++ jonmircha/js/clase34.js
@@ -3,3 +3,10 @@ function contar(a = "vacio"){
     console.log(`La cadena ${a} tiene ${a.length} caracteres`)
   }
   console.log(contar(cadena))
+
+
+  let substring = ("Hola Mundo",4)
+function recortar(a = "vacio",numero = 2){
+    console.log(`La cadena ${a} con el numero de caracteres escogidos ${a.slice(0,numero)}`)
+  }
+  console.log(recortar(substring))
