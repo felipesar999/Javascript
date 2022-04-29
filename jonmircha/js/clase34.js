dc0f1c81368c5588218d6dc5bed7da6565c5479b
Felipe Zapata <soporte@feriadelbrasier.com.co>
2022-04-29 07:07:17 -0500

    clase 34 ej terminados

--- jonmircha/js/clase34.js
+++ jonmircha/js/clase34.js
@@ -1,12 +1,53 @@
-let cadena = prompt("Digita la cadena a estudiar")
+//
+//EJERCICIO1
+/*let cadena = prompt("Digita la cadena a estudiar")
 function contar(a = "vacio"){
+
     console.log(`La cadena ${a} tiene ${a.length} caracteres`)
   }
   console.log(contar(cadena))
-
-
-  let substring = ("Hola Mundo",4)
-function recortar(a = "vacio",numero = 2){
-    console.log(`La cadena ${a} con el numero de caracteres escogidos ${a.slice(0,numero)}`)
+*/
+/*
+//EJERCICIO 2
+  let hola = "Hola Mundo"
+function recortar(a,numero){
+    console.log(`La cadena ${a} al modificarla queda ${a.substring(0,numero)}`)
   }
-  console.log(recortar(substring))
+  recortar(hola,8)
+  
+
+//EJERCICIO 3
+
+function ejer4(str){
+  console.log(str.split(" ")  )
+}
+ejer4("Hola Que Tal Como Te va, bien y a vos")
+
+
+
+//EJERCICIO 4
+function ejer4(str, n = undefined) {
+  if ((n === undefined) || isNaN(n) || (n = "")){
+    console.error("no es un numero")
+    return
+  }
+  if ((n <= 0)) {
+    console.log("Error en el numero ingresado")
+    return
+  } else {
+    let texto = ""
+    try {
+      for (let i = 0; i < n; i++) {
+        texto += str + " "
+        console.log(`consola ${i + 1} de ${str}`)
+      }
+      console.log(texto)
+    }
+    catch (e) {
+      console.error("Error de numero " + e)
+    }
+  }
+}
+ejer4("Hola Mundo", -1)
+
+*/
